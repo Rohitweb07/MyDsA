cd12cee445a115337a14762e2a118865edea57af
Rohitweb07 <84072828+Rohitweb07@users.noreply.github.com>
2022-09-26 14:55:53 +0530

    Create HashMapImpementation.java

---
+++ Hashing/HashMapImpementation.java
@@ -0,0 +1,92 @@
+// Java program to illustrate 
+// Java.util.HashMap 
+
+import java.util.HashMap; 
+import java.util.Map;
+
+public class GFG { 
+    public static void main(String[] args) 
+    { 
+        // Create an empty hash map 
+        HashMap<String, Integer> m  = new HashMap<>(); 
+
+        // Add elements to the map 
+        m.put("gfg", 10); 
+        m.put("ide", 15); 
+        m.put("courses", 20); 
+
+        // Print size and content
+        System.out.println(m); 
+        System.out.println(m.size()); 
+        
+        // Iterating over HashMap 
+        for(Map.Entry<String, Integer>e : m.entrySet())
+            System.out.println(e.getKey() + " " + e.getValue());
+    } 
+} 
+///***************************************************************************////
+// Java program to illustrate 
+// Java.util.HashMap 
+
+import java.util.HashMap; 
+import java.util.Map;
+
+public class GFG { 
+    public static void main(String[] args) 
+    { 
+        // Create an empty hash map 
+        HashMap<String, Integer> m 
+            = new HashMap<>(); 
+
+        // Add elements to the map 
+        m.put("gfg", 10); 
+        m.put("ide", 15); 
+        m.put("courses", 20); 
+
+        // Check for a key
+        if (m.containsKey("ide")) 
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        
+        // Remove key "ide"
+        // and returns the associated value 15
+        m.remove("ide");
+        System.out.println(m.size());
+    } 
+} 
+//-----------------------------------------------------*********************************-------------------------------------
+// Java program to illustrate 
+// Java.util.HashMap 
+
+import java.util.HashMap; 
+import java.util.Map;
+
+public class GFG { 
+    public static void main(String[] args) 
+    { 
+        // Create an empty hash map 
+        HashMap<String, Integer> m 
+            = new HashMap<>(); 
+
+        // Add elements to the map 
+        m.put("gfg", 10); 
+        m.put("ide", 15); 
+        m.put("courses", 20); 
+
+        // Check for a key
+        if (m.containsKey("ide")) 
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        
+        // Remove key "ide"
+        // and returns the associated value 15
+        m.remove("ide");
+        System.out.println(m.size());
+    } 
+} 
+
+
+
+
