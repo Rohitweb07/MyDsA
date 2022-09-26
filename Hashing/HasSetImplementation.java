1a7ec50038c1e705262ccab292b87fa6df3bdd63
Rohitweb07 <84072828+Rohitweb07@users.noreply.github.com>
2022-09-26 14:51:08 +0530

    Create HasSetImplementation.java

---
+++ Hashing/HasSetImplementation.java
@@ -0,0 +1,70 @@
+// Java program to demonstrate 
+// working of HashSet 
+import java.util.*; 
+
+class Test 
+{ 
+    public static void main(String[]args) 
+    { 
+        HashSet<String> h = new HashSet<String>(); 
+
+        
+        h.add("gfg"); 
+        h.add("courses"); 
+        h.add("ide"); 
+
+      
+        System.out.println(h.size());
+        
+        h.remove("ide");
+        System.out.println(h.size());
+        
+        for(String s: h)
+        {
+            System.out.print(s+" ");
+        }
+        
+        
+        
+        System.out.println("\n"+h.isEmpty());
+        
+    } 
+} 
+//-------------------------------***************************-----------------------------------------------
+
+
+// Java program to demonstrate 
+// working of HashSet 
+import java.util.*; 
+
+class Test 
+{ 
+    public static void main(String[]args) 
+    { 
+        HashSet<String> h = new HashSet<String>(); 
+
+        
+        h.add("gfg"); 
+        h.add("courses"); 
+        h.add("ide"); 
+
+      
+        System.out.println(h);
+        
+        
+        System.out.println(h.contains("ide"));
+        
+       
+        Iterator<String> i = h.iterator();
+        
+        while(i.hasNext())
+        {
+            System.out.print(i.next()+" ");
+        }
+        
+        
+    } 
+} 
+
+
+
